42e478de077e2ca58a8db7c2a63eb9b4b60a1381
masahiro331 <m_fujimura@r.recruit.co.jp>
2021-06-22 10:46:40 +0900

    WIP add cyclonedx output

--- pkg/report/cyclonedx.go
+++ pkg/report/cyclonedx.go
@@ -1,14 +1,10 @@
 package report
 
 import (
-	"encoding/json"
 	"fmt"
 	"io"
-	"os"
 	"strings"
 	"time"
-
-	"github.com/aquasecurity/trivy/pkg/log"
 
 	fos "github.com/aquasecurity/fanal/analyzer/os"
 	"github.com/aquasecurity/fanal/types"
@@ -24,27 +20,19 @@ type CycloneDXWriter struct {
 }
 
 // Write writes the results in CycloneDX format
-func (jw CycloneDXWriter) Write(report Report) error {
-	var v interface{} = report
-	if os.Getenv("TRIVY_NEW_JSON_SCHEMA") == "" {
-		// After migrating to the new JSON schema, TRIVY_NEW_JSON_SCHEMA will be removed.
-		log.Logger.Warnf("DEPRECATED: the current JSON schema is deprecated, check %s for more information.",
-			"https://github.com/aquasecurity/trivy/discussions/1050")
-		v = report.Results
-	}
-
-	output, err := json.MarshalIndent(v, "", "  ")
+func (cw CycloneDXWriter) Write(report Report) error {
+	bom, err := convertToBom(report)
 	if err != nil {
-		return xerrors.Errorf("failed to marshal json: %w", err)
+		return xerrors.Errorf("failed to convert to bom: %w", err)
+	}
+	if err = cdx.NewBOMEncoder(cw.Output, cdx.BOMFileFormatJSON).Encode(bom); err != nil {
+		return xerrors.Errorf("failed to encode bom: %w", err)
 	}
 
-	if _, err = fmt.Fprint(jw.Output, string(output)); err != nil {
-		return xerrors.Errorf("failed to write json: %w", err)
-	}
 	return nil
 }
 
-func (r Report) ConvertToBom() (*cdx.BOM, error) {
+func convertToBom(r Report) (*cdx.BOM, error) {
 	bom := cdx.NewBOM()
 	bom.Metadata = &cdx.Metadata{
 		Timestamp: time.Now().Format(time.RFC3339),
@@ -55,20 +43,26 @@ func (r Report) ConvertToBom() (*cdx.BOM, error) {
 				Version: "0.17.1",
 			},
 		},
-
-		// TODO: Support Component section
-		// Component: &cdx.Component{}
+		Component: &cdx.Component{
+			Type:   TypeToComponent(string(r.ArtifactType)),
+			Name:   r.ArtifactName,
+			BOMRef: r.ArtifactID,
+			// TODO: Support properties for cdx 1.3, add repo tags and digests
+		},
 	}
 
 	libraryMap := map[string]struct{}{}
 	componets := []cdx.Component{}
-
+	dependencies := []cdx.Dependency{}
 	for _, result := range r.Results {
 		component := &cdx.Component{
-			Type: TypeToComponent(result.Type),
+			Type:   TypeToComponent(result.Type),
+			Name:   result.Target,
+			BOMRef: result.Target,
 		}
 		componets = append(componets, *component)
 
+		componentDependencies := []cdx.Dependency{}
 		for _, pkg := range result.Packages {
 			purl, err := NewPackageUrl(result.Type, pkg)
 			if err != nil {
@@ -82,14 +76,16 @@ func (r Report) ConvertToBom() (*cdx.BOM, error) {
 				BOMRef:     purl,
 			}
 			if _, ok := libraryMap[libComponent.BOMRef]; !ok {
-				componets = append(componets, *component)
+				componets = append(componets, *libComponent)
 			}
+			componentDependencies = append(componentDependencies, cdx.Dependency{Ref: libComponent.BOMRef})
 		}
-		result.Packages
+		dependencies = append(dependencies, cdx.Dependency{Ref: component.BOMRef, Dependencies: &componentDependencies})
 	}
-	bom.Components = &compoents
+	bom.Components = &componets
+	bom.Dependencies = &dependencies
 
-	return cdx.BOM{}
+	return bom, nil
 }
 
 func NewPackageUrl(t string, pkg types.Package) (string, error) {
@@ -110,29 +106,17 @@ func NewPackageUrl(t string, pkg types.Package) (string, error) {
 	return purl, nil
 }
 
-/*
-type Package struct {
-	Name            string `json:",omitempty"`
-	Version         string `json:",omitempty"`
-	Release         string `json:",omitempty"`
-	Epoch           int    `json:",omitempty"`
-	Arch            string `json:",omitempty"`
-	SrcName         string `json:",omitempty"`
-	SrcVersion      string `json:",omitempty"`
-	SrcRelease      string `json:",omitempty"`
-	SrcEpoch        int    `json:",omitempty"`
-	Modularitylabel string `json:",omitempty"`
-	Layer           Layer  `json:",omitempty"`
-}
-*/
-
 func TypeToComponent(t string) (c cdx.ComponentType) {
 	switch t {
+	case string(types.ArtifactContainerImage):
+		return cdx.ComponentTypeContainer
+	case string(types.ArtifactFilesystem), string(types.ArtifactRemoteRepository):
+		return cdx.ComponentTypeApplication
 	case fos.RedHat, fos.Debian, fos.Ubuntu, fos.CentOS, fos.Fedora, fos.Amazon,
 		fos.Oracle, fos.Windows, fos.OpenSUSE, fos.OpenSUSELeap, fos.OpenSUSETumbleweed,
 		fos.SLES, fos.Photon, fos.Alpine:
 		return cdx.ComponentTypeOS
 	default:
-		return cdx.ComponentTypeApplication
+		return cdx.ComponentTypeFile
 	}
 }
